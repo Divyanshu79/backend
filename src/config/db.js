e16d07bb933c93d467024fb522af6fbe4358e188
Divyanshu79 <divyanshur934@gmail.com>
2024-08-06 17:15:11 +0530

    mongo db connected

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,54 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
 
 
-let connectDB = async () => {
+
+const connectDB = async () => {
     try {
-
-        const dbconnection = mongoose.connect(process.env.MONGO)
-        console.log(`database is connected successfully `)
-
-    } catch (error) {
-        console.log(error)
+        const dbConnection = await mongoose.connect(`${process.env.MONGO}/${DB_NAME}`)
+        console.log(`Database is connectd successfully!! DB HOST ${dbConnection.connection.host}`)
 
     }
-};
+    catch (error) {
+        console.log("Error", error);
+        process.exit(1);
+        // throw error;
+
+    }
+}
+
+
 export default connectDB;
+
+
+
+
+
+
+
+
+
+
+// This is efis concept to immediately execute the function and we put semicoln        before using efis it may be that there is missig semicoln in previous statement ; ()()
+/*
+(async () => {
+    try {
+        const connectDB = await mongoose.connect(`${process.env.MONGO} / ${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR", error)
+            throw error
+
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+        throw error;
+
+    }
+})()
+
+
+*/
